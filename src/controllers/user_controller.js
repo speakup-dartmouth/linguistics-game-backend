6e86f873718d68b19168a8ae891841099fb97d05
Isabella Hochschild <ihochschild@gmail.com>
2022-05-18 14:15:41 -0400

    add auth

---
+++ src/controllers/user_controller.js
@@ -0,0 +1,35 @@
+import jwt from 'jwt-simple';
+import dotenv from 'dotenv';
+import User from '../models/user_model';
+
+dotenv.config({ silent: true });
+
+export const signin = (user) => {
+  return tokenForUser(user);
+};
+
+// note the lovely destructuring here indicating that we are passing in an object with these 3 keys
+export const signup = async ({ email, password }) => {
+  if (!email || !password) {
+    throw new Error('You must provide email and password');
+  }
+
+  // See if a user with the given email exists
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    // If a user with email does exist, return an error
+    throw new Error('Email is in use');
+  }
+
+  const user = new User();
+  user.email = email;
+  user.password = password;
+  await user.save();
+  return tokenForUser(user);
+};
+
+// encodes a new token for a user object
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, process.env.AUTH_SECRET);
+}
