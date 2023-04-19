910e9174029a606ed2686b3f586c8f76625089fc
gracewanggw <grace.wang.24@dartmouth.edu>
2023-04-19 13:46:41 -0400

    remove API client key validation

--- src/common/auth_utils.js
+++ src/common/auth_utils.js
@@ -1,14 +1,14 @@
 /* eslint-disable import/prefer-default-export */
-export const clientApiKeyValidation = async (req, res, next) => {
-  const clientApiKey = req.get('API_KEY');
-  if (clientApiKey !== process.env.API_KEY) {
-    return res.status(400).send({
-      status: false,
-      response: 'Please provide a valid API Key',
-    });
-  }
-  return next();
-};
+// export const clientApiKeyValidation = async (req, res, next) => {
+//   const clientApiKey = req.get('API_KEY');
+//   if (clientApiKey !== process.env.API_KEY) {
+//     return res.status(400).send({
+//       status: false,
+//       response: 'Please provide a valid API Key',
+//     });
+//   }
+//   return next();
+// };
 
 // https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
 export const validateEmail = (email) => {
